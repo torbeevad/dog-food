f6c4691020f862981dc9f4e2c1a9772bc0fa839e
Алексей Торбеев <torbec@yandex.ru>
2023-06-27 12:11:29 +0300

    fix

--- src/Pages/CatalogPage/CatalogPage.jsx
+++ src/Pages/CatalogPage/CatalogPage.jsx
@@ -3,22 +3,22 @@ import {Catalog} from "../../Components/Catalog/Catalog";
 import {Sort} from "../../Components/Sort/Sort";
 import {QueryFind} from "../../Components/QueryFind/QueryFind";
 import {useSelector} from "react-redux";
-import {Loader} from "../../Components/Loader/Loader";
+import {Page404} from "../Page404/Page404";
 
 export const CatalogPage = () => {
 
-    const {allProducts, loading} = useSelector(s => s.products)
+    const {allProducts} = useSelector(s => s.products)
 
     return (
         <>
             < QueryFind cards={allProducts}/>
-            {allProducts?.length !== 0 && !loading ?
+            {allProducts?.length !== 0 ?
                 <>
                     <Sort/>
                     <Catalog items={allProducts}/>
                 </>
                 :
-                <Loader/>
+                <Page404/>
             }
         </>
     )
