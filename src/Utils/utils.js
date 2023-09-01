a9c732f83eb5de5f3a9c04ce25bd42a8b917958a
Алексей Торбеев <torbec@yandex.ru>
2023-09-01 13:18:41 +0300

    fix

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -2,9 +2,13 @@ import {addLike, deleteLike} from "./api/apiProducts";
 
 export const url = "https://api.react-learning.ru"
 
+// export const headers = {
+//     "Content-Type": "application/json",
+//     authorization: `${localStorage.getItem("token")}`
+// }
 export const headers = {
     "Content-Type": "application/json",
-    authorization: `${localStorage.getItem("token")}`
+    authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDc0YmUzNWUwYmYyYzUxOWJjM2EwNDEiLCJncm91cCI6Imdyb3VwLTEyIiwiaWF0IjoxNjkzNTYxOTcwLCJleHAiOjE3MjUwOTc5NzB9.QpKpRGNOBrAZ52znDmwvUNQR_QFp7qru-Z6oiVjpZec"
 }
 
 export const changeProductLike = async (id, isLiked) => {
